0acbd083ec5326e224a81a0412480b9fbff8029c
mohamamd sbaih <mohammad_saleh_sbaih@outlook.com>
2021-12-05 07:47:14 +0200

    End Of Section 6

--- ZwajApp-SPA/src/app/_services/auth.service.ts
+++ ZwajApp-SPA/src/app/_services/auth.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from "rxjs/operators";
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  jwtHelper = new JwtHelperService();
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  decodedToken:any;
 
 constructor(private http:HttpClient) { }
 
@@ -17,12 +21,25 @@ login(model:any){
      map((response:any)=>{
      const user = response;
      if(user){localStorage.setItem('token',user.token);
+     this.decodedToken = this.jwtHelper.decodeToken(user.token);
+     console.log(this.decodedToken);
     }
   }))
 
 }
 register(model:any){
   return this.http.post(this.baseUrl+'register',model);
+}
+
+loggedIn(){
+
+  try{
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+  catch{
+    return false
+  }
 }
 
 }
